1ef9d8191a3f897b98bc7f7b21b187dc87180403
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-08-24 19:06:43 +0300

    complete

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback){
+    return callback();
+}
+function returnsANamedFunction(){
+    return function namedFunction(something){
+        return something;
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(something){
+        return something;
+    }
+}
